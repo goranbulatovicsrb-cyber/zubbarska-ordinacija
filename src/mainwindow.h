b14ce202b12e8e609ae81fb5cbf5198c89ef9f66
goranbulatovicsrb-cyber <goranbulatovicsrb@gmail.com>
2026-03-12 15:11:02 +0100

    Add files via upload

---
+++ src/mainwindow.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <QMainWindow>
+#include "models.h"
+
+class QListWidget;
+class QListWidgetItem;
+class QLineEdit;
+class QStackedWidget;
+class QLabel;
+class QTabWidget;
+class QTableWidget;
+class QDateEdit;
+class QComboBox;
+class PatientHistoryWidget;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private slots:
+    void onAddPatient();
+    void onEditPatient();
+    void onDeletePatient();
+    void onPatientSelected(QListWidgetItem* item);
+    void onSearchPatients(const QString& text);
+    void onAddIntervention();
+    void onEditIntervention(int id);
+    void onDeleteIntervention(int id);
+    void onViewPdf(const QString& path);
+    void onPrintPatientReport();
+    void onPrintIntervention();
+    void onSearch();
+    void onSearchResultDoubleClicked(int row, int col);
+    void refreshDashboard();
+    void onAddAppointment();
+
+private:
+    void setupUi();
+    void setupMenu();
+    void setupToolbar();
+    QWidget* buildLeftPanel();
+    QWidget* buildRightPanel();
+    void setupDashboard();
+    void setupPatientsTab();
+    void setupSearchTab();
+    void setupAppointmentsTab();
+
+    void loadPatientList();
+    void selectPatientById(int id);
+    void refreshPatientDetail();
+    void showPatientCard(const Patient& p);
+
+    // UI refs
+    QListWidget*          m_patientList        = nullptr;
+    QLineEdit*            m_searchBox          = nullptr;
+    QStackedWidget*       m_rightStack         = nullptr;
+
+    QWidget*              m_dashWidget         = nullptr;
+    QLabel*               m_statPatients       = nullptr;
+    QLabel*               m_statIvMonth        = nullptr;
+    QLabel*               m_statRevMonth       = nullptr;
+    QTableWidget*         m_upcomingTable      = nullptr;
+
+    QWidget*              m_detailWidget       = nullptr;
+    QTabWidget*           m_detailTabs         = nullptr;
+    QLabel*               m_patientNameLbl     = nullptr;
+    QLabel*               m_patientInfoLbl     = nullptr;
+    QTableWidget*         m_allIvTable         = nullptr;
+    PatientHistoryWidget* m_historyWidget      = nullptr;
+
+    QWidget*              m_searchWidget       = nullptr;
+    QLineEdit*            m_searchName         = nullptr;
+    QDateEdit*            m_searchFrom         = nullptr;
+    QDateEdit*            m_searchTo           = nullptr;
+    QComboBox*            m_searchType         = nullptr;
+    QTableWidget*         m_searchResults      = nullptr;
+    QList<Intervention>   m_searchIvs;
+
+    QWidget*              m_appointmentsWidget = nullptr;
+    QTableWidget*         m_appointTable       = nullptr;
+
+    Patient               m_currentPatient;
+};
